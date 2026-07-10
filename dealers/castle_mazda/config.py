b4ff89672ef127482155f243b2c8fdecf390ecf1
joezsharko <joezsharko@users.noreply.github.com>
2026-07-10 11:14:38 -0500

    Create config.py

---
+++ dealers/castle_mazda/config.py
@@ -0,0 +1,9 @@
+"""Dealer metadata for Castle Mazda Downers Grove."""
+
+DEALER_INFO = {
+    "name": "Castle Mazda Downers Grove",
+    "city": "Downers Grove",
+    "state": "IL",
+    "zip_code": "60515",
+    "make": "Mazda",
+}
