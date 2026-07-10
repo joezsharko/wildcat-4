7fadfd9f9f7d9e022ba479be24e12fbf6fe83179
joezsharko <joezsharko@users.noreply.github.com>
2026-07-10 10:40:20 -0500

    Create config.py

---
+++ dealers/mcgrath_mazda/config.py
@@ -0,0 +1,9 @@
+"""Dealer metadata for McGrath City Mazda."""
+
+DEALER_INFO = {
+    "name": "McGrath City Mazda",
+    "city": "Chicago",
+    "state": "IL",
+    "zip_code": "60707",
+    "make": "Mazda",
+}
